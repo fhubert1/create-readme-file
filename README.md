acc11139226fc863e9a021eefc370c59f84a0151
fhubert1 <f.hubert.712@gmail.com>
2024-04-18 17:48:16 -0400

    add line feeds and new license selections that include badge and info text

--- README.md
+++ README.md
@@ -1,39 +1,47 @@
-    # Generate README File
+    
+# Generate README Project
 
-    ## Description
+## Description
 
-    generate a professional looking readme file using inquirer and node.js
+Use node.js and inquirer to generate a professional looking README file
 
-    ## Table of Contents
-       - [Installation](#installation)
-       - [Usage](#usage)
-       - [Credits](#credits)
-       - [License](#license)
-       - [Contributing](#contributing)
-       - [Tests](#tests)
-       - [Questions](#questions)
+## Table of Contents
+   - [Installation](#installation)
+   - [Usage](#usage)
+   - [Credits](#credits)
+   - [License](#license)
+   - [Contributing](#contributing)
+   - [Tests](#tests)
+   - [Questions](#questions)
 
 
-    ## Installation
-    no installation instructions
+## Installation
+Run node index.js from terminal window and answer the prompts
 
-    ## Usage
-    no usage
+## Usage
+video submission will show how to use the application
 
-    ## Credits
+## Credits
     
 
-    ## License
-    ISC License
+## License
 
-    ## Contributing
-    no contributors
+This project is licensed under the ISC License - see the [LICENSE](LICENSE) file for details.
 
-    ## Tests
-    no testing
+ 
 
-    ## Questions
-    [Contact Me](mailto:f.hubert.712@gmail.com)
-    [Git](https://github.com/fhubert1)
 
-    
+[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC) 
+
+
+## Contributing
+submit suggestions to email address listed below
+
+## Tests
+No test cases created for now
+
+## Questions
+[Contact Me](mailto:f.hubert.712@gmail.com) 
+
+
+[Git](https://github.com/fhubert1)
--- index.js
+++ index.js
@@ -3,48 +3,87 @@ const inquirer = require('inquirer');
 const prompt = inquirer.createPromptModule();
 const fileName = 'README.md';
 
+const licenseOptions = [
+    {
+        name: 'MIT License',
+        badge: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+        info: `## License\n\nThis project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.\n\n`
+      },
+      {
+        name: 'GNU General Public License (GPL)',
+        badge: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
+        info: `## License\n\nThis project is licensed under the GNU General Public License v3.0 - see the [LICENSE](LICENSE) file for details.\n\n`
+      },
+      {
+        name: `Apache License 2.0`,
+        badge: `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`,
+        info: `## License\n\nThis project is licensed under the Apache License 2.0 - see the [LICENSE](LICENSE) file for details.\n\n`
+      },
+      {
+        name: `ISC License`,
+        badge: `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`,
+        info: `## License\n\nThis project is licensed under the ISC License - see the [LICENSE](LICENSE) file for details.\n\n`
+      },
+      {
+        name: `Mozilla Public License (MPL) 2.0`,
+        badge: `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`,
+        info: `## License\n\nThis project is licensed under the Mozilla Public License (MPL) 2.0 - see the [LICENSE](LICENSE) file for details.\n\n`
+      },
+      {
+        name: `Unlicense`,
+        badge: `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`,
+        info: `This is free and unencumbered software released into the public domain. For more information, please refer to <http://unlicense.org/>` 
+      }
+
+]
+
+
 const generateReadME = ({title, description, installation, usage, contributor, testing, license, username, email}) => {
+    const selectedLicense = licenseOptions.find(option => option.name === license);
+    //console.log(selectedLicense);
     return `    
-    # ${title}
+# ${title}
 
-    ## Description
+## Description
 
-    ${description}
+${description}
 
-    ## Table of Contents
-       - [Installation](#installation)
-       - [Usage](#usage)
-       - [Credits](#credits)
-       - [License](#license)
-       - [Contributing](#contributing)
-       - [Tests](#tests)
-       - [Questions](#questions)
+## Table of Contents
+   - [Installation](#installation)
+   - [Usage](#usage)
+   - [Credits](#credits)
+   - [License](#license)
+   - [Contributing](#contributing)
+   - [Tests](#tests)
+   - [Questions](#questions)
 
 
-    ## Installation
-    ${installation}
+## Installation
+${installation}
 
-    ## Usage
-    ${usage}
+## Usage
+${usage}
 
-    ## Credits
+## Credits
     
 
-    ## License
-    ${license}
+${selectedLicense.info} \n
 
-    ## Contributing
-    ${contributor}
+${selectedLicense.badge} \n
 
-    ## Tests
-    ${testing}
+## Contributing
+${contributor}
 
-    ## Questions
-    [Contact Me](mailto:${email})
-    [Git](https://github.com/${username})
-    `
+## Tests
+${testing}
+
+## Questions
+[Contact Me](mailto:${email}) \r\n
+
+[Git](https://github.com/${username})
+`
+
 }
-
 
 const questions = [
     {
@@ -87,15 +126,16 @@ const questions = [
         type: 'list',
         message: 'Choose a license for your project',
         name: 'license',
-        choices: [
-            'MIT License',
-            'GNU General Public License (GPL)',
-            'Apache License 2.0',
-            'ISC License',
-            'Mozilla Public License 2.0 (MPL 2.0)',
-            'Creative Common Licenses',
-            'Unlicense'
-        ]
+        choices: licenseOptions.map(option => option.name)
+        // [
+        //     'MIT License',
+        //     'GNU General Public License (GPL)',
+        //     'Apache License 2.0',
+        //     'ISC License',
+        //     'Mozilla Public License 2.0 (MPL 2.0)',
+        //     'Creative Common Licenses',
+        //     'Unlicense'
+        // ]
       },  
       {
         type: 'input',
@@ -121,7 +161,9 @@ const questions = [
 
 prompt(questions) 
   .then((responses) =>{
-    //console.log(generateReadME(responses));
+    // const selectedLicense = licenseOptions.find(option => option.name === responses.license);
+    // console.log(selectedLicense);
+    //console.log(responses);
     fs.writeFile(fileName, generateReadME(responses), (err) => {
         if (err) {
             throw err;
